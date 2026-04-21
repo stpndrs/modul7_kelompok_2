6886aa80743759d9fd51387ab920cedafbd182d2
ravinhub <muhammadrizkyramadhan365@gmail.com>
2026-04-21 20:13:33 +0700

    menambahkan class mahasiswa103082400014

---
+++ DataMahasiswa103082400014.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json;
+
+public class Address
+{
+    public string streetAddress { get; set; }
+    public string city { get; set; }
+    public string state { get; set; }
+}
+
+public class Course
+{
+    public string code { get; set; }
+    public string name { get; set; }
+}
+
+public class DataMahasiswa103082400014
+{
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public string gender { get; set; }
+    public int age { get; set; }
+    public Address address { get; set; }
+    public List<Course> courses { get; set; }
+
+    public void ReadJSON()
+    {
+        string json = File.ReadAllText("jurnal7_1_103082400014.json");
+
+        var data = JsonSerializer.Deserialize<DataMahasiswa103082400014>(json);
+
+        Console.WriteLine("Nama: " + data.firstName + " " + data.lastName);
+        Console.WriteLine("Gender: " + data.gender);
+        Console.WriteLine("Age: " + data.age);
+
+        Console.WriteLine("\nAlamat:");
+        Console.WriteLine("Street: " + data.address.streetAddress);
+        Console.WriteLine("City: " + data.address.city);
+        Console.WriteLine("State: " + data.address.state);
+
+        Console.WriteLine("\nCourses:");
+
+        if (data.courses != null)
+        {
+            for (int i = 0; i < data.courses.Count; i++)
+            {
+                Console.WriteLine(data.courses[i].code + " - " + data.courses[i].name);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        DataMahasiswa103082400014 data = new DataMahasiswa103082400014();
+        data.ReadJSON();
+    }
+}
